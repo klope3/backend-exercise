e1e2a218e83d7b6673f815c31f8447b763a10fdd
Josh Klope <klope3@msn.com>
2023-06-02 18:28:12 -0500

    Add status code constants; add basic auth endpoint

--- src/backend.ts
+++ src/backend.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { prisma } from "../prisma/db.setup.js";
 import { validateRequest } from "zod-express-middleware";
 import { z } from "zod";
-import {} from "zod/";
+import bcrypt from "bcrypt";
+import { BAD_REQUEST, FORBIDDEN, NOT_FOUND, OK } from "./statusCodes.js";
 
 const app = express();
 app.use(express.json());
@@ -13,13 +14,13 @@ app.get("/", (req, res) => {
 
 app.get("/users", async (req, res) => {
   const users = await prisma.user.findMany();
-  res.status(200).send(users);
+  res.status(OK).send(users);
 });
 
 app.get("/users/:id", async (req, res) => {
   const id = +req.params.id;
   if (isNaN(id)) {
-    return res.status(400).send({ message: "Bad input" });
+    return res.status(BAD_REQUEST).send({ message: "Bad input" });
   } //should find a way to use zod for this
   const user = await prisma.user.findFirst({
     where: {
@@ -27,9 +28,41 @@ app.get("/users/:id", async (req, res) => {
     },
   });
   if (!user) {
-    return res.status(404).send({ message: "No such user" });
+    return res.status(NOT_FOUND).send({ message: "No such user" });
   }
-  res.status(200).send(user);
+  res.status(OK).send(user);
 });
+
+app.post(
+  "/auth/login",
+  validateRequest({
+    body: z.object({
+      email: z.string(),
+      password: z.string(),
+    }),
+  }),
+  async (req, res) => {
+    const user = await prisma.user.findFirst({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (!user) {
+      return res.status(NOT_FOUND).send({ message: "User not found" });
+    }
+
+    const correctPassword = await bcrypt.compare(
+      req.body.password,
+      user.passwordHash
+    );
+
+    if (!correctPassword) {
+      return res.status(FORBIDDEN).send({ message: "Invalid password" });
+    }
+
+    res.status(OK).send({ message: "Authenticated" });
+  }
+);
 
 app.listen(3000);
